3937bc6706cd0b798ca152b754b569f3ff9ff1f4
Bruno Kakazu <43326668+BrunoKakazu@users.noreply.github.com>
2026-06-21 16:35:18 -0400

    Mapas e Botoes
    Mapas e botões já estão colocados

--- Assets/_Project/Scripts/Cards/DeckScript.cs
+++ Assets/_Project/Scripts/Cards/DeckScript.cs
@@ -7,18 +7,6 @@ public class DeckScript : MonoBehaviour
     public List<CardData> cardDatas;
     [SerializeField] private GameObject cardPrefab;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
         public CardData randomGenerate()
     {
         int randI = Random.Range(1, cardDatas.Count);
